5d722ae9edbf917d9b348bced0092817e55b0af0
William Victor <vicken408@gmail.com>
2026-01-30 16:33:17 +0100

    horizon

--- resources/views/competitor_followers/index.blade.php
+++ resources/views/competitor_followers/index.blade.php
@@ -104,7 +104,7 @@
 </div>
 
 <!-- Delete Confirmation Modal -->
-<div id="delete-audience-modal" class="fixed inset-0 bg-black bg-opacity-50 z-50 hidden flex items-center justify-center">
+<div id="delete-audience-modal" class="fixed inset-0 bg-black/50 z-50 hidden flex items-center justify-center h-screen">
     <div class="bg-white rounded-lg shadow-xl max-w-md w-full mx-4">
         <div class="p-6">
             <h3 class="text-lg font-semibold text-gray-900 mb-4">Delete Competitor Audience</h3>
