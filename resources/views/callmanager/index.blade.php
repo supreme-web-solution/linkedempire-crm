6512d2c9d9788aec5f3696b31bcdf5bc8875f6c5
William Victor <vicken408@gmail.com>
2026-02-10 14:03:15 +0100

    update

--- resources/views/callmanager/index.blade.php
+++ resources/views/callmanager/index.blade.php
@@ -3,19 +3,24 @@
 @section('content')
 <div class="mt-4">
     <div class="border-b border-gray-200 bg-white rounded-lg">
-        <nav class="flex gap-x-4" aria-label="Tabs" role="tablist" aria-orientation="horizontal">
-            <a href="{{route('calls')}}" class="hs-tab-active:font-semibold hs-tab-active:border-blue-600 hs-tab-active:text-blue-600 py-4 px-2 inline-flex items-center gap-x-2 border-b-2 border-transparent text-sm whitespace-nowrap text-gray-500 hover:text-blue-600 focus:outline-hidden focus:text-blue-600 disabled:opacity-50 disabled:pointer-events-none active" id="tabs-with-underline-item-1" aria-selected="true" data-hs-tab="#tabs-with-underline-1" aria-controls="tabs-with-underline-1" role="tab">
-                Call Status 
-                </a>
-            <a href="{{route('calls.reminders')}}" class="hs-tab-active:font-semibold hs-tab-active:border-blue-600 hs-tab-active:text-blue-600 py-4 px-2 inline-flex items-center gap-x-2 border-b-2 border-transparent text-sm whitespace-nowrap text-gray-500 hover:text-blue-600 focus:outline-hidden focus:text-blue-600 disabled:opacity-50 disabled:pointer-events-none" id="tabs-with-underline-item-2" aria-selected="false" data-hs-tab="#tabs-with-underline-2" aria-controls="tabs-with-underline-2" role="tab">
+        <nav class="flex flex-wrap gap-x-4" aria-label="Tabs" role="tablist" aria-orientation="horizontal">
+            <button type="button" class="hs-tab-active:font-semibold hs-tab-active:border-blue-600 hs-tab-active:text-blue-600 py-4 px-2 inline-flex items-center gap-x-2 border-b-2 border-transparent text-sm whitespace-nowrap text-gray-500 hover:text-blue-600 focus:outline-hidden focus:text-blue-600 disabled:opacity-50 disabled:pointer-events-none active" id="tabs-with-underline-item-1" aria-selected="true" data-hs-tab="#tabs-with-underline-1" aria-controls="tabs-with-underline-1" role="tab">
+                Call Campaigns
+            </button>
+            <button type="button" class="hs-tab-active:font-semibold hs-tab-active:border-blue-600 hs-tab-active:text-blue-600 py-4 px-2 inline-flex items-center gap-x-2 border-b-2 border-transparent text-sm whitespace-nowrap text-gray-500 hover:text-blue-600 focus:outline-hidden focus:text-blue-600 disabled:opacity-50 disabled:pointer-events-none" id="tabs-with-underline-item-2" aria-selected="false" data-hs-tab="#tabs-with-underline-2" aria-controls="tabs-with-underline-2" role="tab">
+                Call Status
+            </button>
+            <a href="{{route('calls.reminders')}}" class="py-4 px-2 inline-flex items-center gap-x-2 border-b-2 border-transparent text-sm whitespace-nowrap text-gray-500 hover:text-blue-600 focus:outline-hidden focus:text-blue-600">
                 Call Reminders
-                </a>
+            </a>
         </nav>
     </div>
     <div class="mt-3">
         <div id="tabs-with-underline-1" role="tabpanel" aria-labelledby="tabs-with-underline-item-1">
-            @include('callmanager.call-status')
             @include('callmanager.call-campaigns')
+        </div>
+        <div id="tabs-with-underline-2" class="hidden" role="tabpanel" aria-labelledby="tabs-with-underline-item-2">
+            @include('callmanager.call-status')
         </div>
     </div>
 </div>
